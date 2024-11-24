5cf02b8be7499e5bf6fea2f3d262a1f60367a932
Emmett Verba <emmettverba@gmail.com>
2024-11-24 15:35:07 -0800

    testing deployment bug

--- app/api/uploadthing/route.ts
+++ app/api/uploadthing/route.ts
@@ -5,6 +5,10 @@ import { ourFileRouter } from "./core";
 // Export routes for Next App Router
 export const { GET, POST } = createRouteHandler({
     router: ourFileRouter,
+    config: {
+        token: process.env.UPLOADTHING_TOKEN
+    }
+
 
     // Apply an (optional) custom config:
     // config: { ... },
